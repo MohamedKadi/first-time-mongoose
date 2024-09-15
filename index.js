5c36b6ae8803f0d5a0f141e92194f1a0d4a3a895
MohamedKadi <gamesmohamed14@gmail.com>
2024-09-15 09:52:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    console.log("Connected");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+  });
