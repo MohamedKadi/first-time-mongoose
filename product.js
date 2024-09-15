884e04256ab79059202ffc1500aa146c93fdee0a
MohamedKadi <gamesmohamed14@gmail.com>
2024-09-15 13:05:02 +0100

    more validation

--- product.js
+++ product.js
@@ -10,18 +10,41 @@ async function main() {
 
 const productSchema = new mongoose.Schema({
     name: {
+        //schematypes
         type: String,
         required: true,
+        maxlength: 20,
     },
     price: {
         type: Number,
-        required: true
+        required: true,
+        min: 1
+    },
+    onSale: {
+        type: Boolean,
+        default: false,
+    },
+    //cetegories: [String]
+    cetegories: {
+        type: String,
+        default: ['cycling']
+    },
+    qty: {
+        online: {
+            type: Number,
+            default: 0,
+        },
+        inStore: {
+            type: Number,
+            default: 0,
+        }
+
     }
 })
 
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({name: 'Mountain bike', price: 299});
+const bike = new Product({name: 'Mountain bike', price: 299, categories:['cycling','safety']});
 bike.save()
 .then(data=>{
     console.log(data);
